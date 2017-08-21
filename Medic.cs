63a41e5055e4d033c026320f50c19290e2da0f01
Robby Bourne <robbybourne1@gmail.com>
2017-08-21 14:56:34 -0400

    Interface solidier types

--- Medic.cs
+++ Medic.cs
@@ -1,6 +1,23 @@
 namespace igiDaily_dotnet
 {
-    public class Medic : IMachinegun, IPistol, ISniper
+    public class Medic : Soldier, IWeapon
     {
+        public Medic(string name, string rank, int serialNumber) : base(name, rank, serialNumber)
+        {
+        }
 
+        public void Reload()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Shoot()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Speak()
+        {
+            throw new System.NotImplementedException("Medic is Here");
+        }
     }
