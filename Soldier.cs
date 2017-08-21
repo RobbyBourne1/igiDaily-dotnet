212159db42a440cd6e336c1b5d3f139f5002c19f
Robby Bourne <robbybourne1@gmail.com>
2017-08-21 14:12:12 -0400

    Set up Solldier Class

---
+++ Soldier.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace igiDaily_dotnet
+{
+    public abstract class Soldier
+    {
+        public string Name { get; set; }
+        public string Rank { get; set; }
+        public int SerialNumber { get; set; }
+
+        public Soldier(string name, string rank, int serialNumber)
+        {
+            Name = name;
+            Rank = rank;
+            SerialNumber = serialNumber;
+        }
+
+        public abstract void Speak();
+
+        public void Sleep()
+        {
+            Console.WriteLine("Snore.....ZZzzzzz");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Yummmmm");
+        }
+
+        public void March()
+        {
+            Console.WriteLine("Left...Left..Left, Right, Left")
+        }
+
+    }
+}
